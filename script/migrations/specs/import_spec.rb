a2ba95e56d14892b6238b860ccfa336e370a76df
Gerhard Schlager <gerhard.schlager@discourse.org>
2023-10-11 22:58:16 +0200

    Initial structure

---
+++ script/migrations/specs/import_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+RSpec.describe "Migrations::Import" do
+  subject(:cli) { system "script/migrations/import" }
+
+  it "works" do
+    expect { cli }.to output(include("Hello, world!")).to_stdout_from_any_process
+  end
+end
