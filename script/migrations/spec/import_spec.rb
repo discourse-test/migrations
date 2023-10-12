684525d1be07ab00433c13dc55e2660b7815852a
Gerhard Schlager <gerhard.schlager@discourse.org>
2023-10-12 22:12:14 +0200

    a small change

--- script/migrations/spec/import_spec.rb
+++ script/migrations/spec/import_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Migrations::Import" do
 
   it "works" do
     expect { cli }.to output(
-      include("Importing into Discourse #{Discourse::VERSION::STRING}"),
+      include("Importing into Discourse #{Discourse::VERSION::STRING}!!"),
     ).to_stdout_from_any_process
   end
 end
