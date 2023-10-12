552109c9d47591938cb09a6b1d764b9010f09c6d
Gerhard Schlager <gerhard.schlager@discourse.org>
2023-10-12 22:08:45 +0200

    Test GitHub Actions

--- script/migrations/specs/import_spec.rb
+++ script/migrations/specs/import_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe "Migrations::Import" do
   subject(:cli) { system "script/migrations/import" }
 
   it "works" do
-    expect { cli }.to output(include("Hello, world!")).to_stdout_from_any_process
+    expect { cli }.to output(
+      include("Importing into Discourse #{Discourse::VERSION::STRING}"),
+    ).to_stdout_from_any_process
   end
 end
